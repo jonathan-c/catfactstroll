6e1ad60de8d0722d6ee47f43cc004becf9f58363
Jonathan Castillo <joncastillo123@gmail.com>
2013-06-20 11:33:16 -0400

    got javascript to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def index
     @catfact = Catfact.order("RANDOM()").first
+    @replies = Catfact.replies
   end
   
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Catfacts::Application.configure do
 
   # Do not compress assets
   config.assets.compress = false
+  
+  config.assets.precompile += %w( *.js *.css )
 
   # Expands the lines which load the assets
   config.assets.debug = true
