574efea8652858424fce698beb08ea969935cfe8
Jonathan Castillo <joncastillo123@gmail.com>
2013-07-03 08:47:41 -0400

    fixed syntax and edited catfacts#create

--- app/controllers/catfacts_controller.rb
+++ app/controllers/catfacts_controller.rb
@@ -1,11 +1,12 @@
 class CatfactsController < ApplicationController
   def create
-    if check_if_fact_exists(params[:message]).nil?
-      @catfact = Catfact.find_by_message(params[:message])
-    else
-      @catfact = Catfact.new(message: params[:message])
-      @catfact.save
-    end
+    # if check_if_fact_exists(params[:message]).nil?
+    #   @catfact = Catfact.find_by_message(params[:message])
+    # else
+    #   @catfact = Catfact.new(message: params[:message])
+    #   @catfact.save
+    # end
+    @catfact = Catfact.find_or_create_by_message(params[:message])
     @catfact.deliver(params[:to])
     redirect_to root_path, :notify => "Sent!"
   end
